08d729e21e96f97a8b1251209f4be95fbb5ff487
Harnoorkaur06 <harnoor2925.beai24@chitkara.edu.in>
2026-07-15 19:22:54 +0530

    day7  updated

--- phase_01_htmlcssjs/day_07/assign1/recipes.js
+++ phase_01_htmlcssjs/day_07/assign1/recipes.js
@@ -2,28 +2,40 @@
 
 let cardContainer=document.getElementById("cardsContainer")
 
-let url='https://dummyjson.com/recipes'
-let fetchData=async()=>{
-    let response= await fetch(url)
+// let url='https://dummyjson.com/recipes/search?q=${value}'
+let fetchData=async(value="")=>{
+    let response= await fetch(`https://dummyjson.com/recipes/search?q=${value}`)
     let data=await response.json()
     
     let arr=data.recipes
-
+    cardContainer.innerHTML=""
     arr.forEach((element , i )=> {
         // console.log(arr[i].name)
         cardContainer.innerHTML+=`
         <div id="d-${i}">
             <img src="${element.image}">
             <h3>${element.name}</h3>
-            <p>Timne:${element.prepTimeMinutes}</p>
-            <button>Delete</button>
+            <p>Timne:${element.prepTimeMinutes} Minutes</p>
+            <button onclick="handleDelete(${i})">Delete</button>
         </div>
         `
     });
 
 
 }
-// fetchData()
+fetchData()
+
+function handleDelete(divId){
+    // alert(`trigered ${divId}`)
+    let div=document.getElementById(`d-${divId}`)
+    div.remove()
+}
+
+function searchRecipe(){
+    let inp=document.getElementById("inp").value
+    fetchData(inp)
+    document.getElementById("inp").value=""
+}
 
 // diff between normal func and arroe func
-// how hoisting behaves in both
+// how hoisting behaves in both         

5f5b2d1e2e9a1ff9e79e88d9a0f1041f10364bec
Harnoorkaur06 <harnoor2925.beai24@chitkara.edu.in>
2026-07-17 17:08:31 +0530

    day7  updated

---
+++ phase_01_htmlcssjs/day_07/assign2/index.js
@@ -0,0 +1,13 @@
+// map--> returns Array
+// ForEach--> print single elements
+
+// HOF---> foreach, map,filter, reduce, sort
+
+// filter--> returns array
+    //   --> used for extraction of data
+    //   --> used in react to delete elements
+
+// reduce -->returns single value after doing some mathematical operations
+
+// accumilator in reduce is starting value
+

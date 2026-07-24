cf45851dad44b260345eca4e70e667149d665b23
Harnoorkaur06 <harnoor2925.beai24@chitkara.edu.in>
2026-07-24 17:46:46 +0530

    day9 added

---
+++ phase_01_htmlcssjs/day_09/array_methods.js
@@ -0,0 +1,64 @@
+// foreach, map,filter, reduce, sort --> hof, and also inbuild array methods
+
+// let arr=["Javascript","HTML","CSS"]
+// console.log(arr[1])
+
+// // length
+// console.log(arr.length)
+
+// // toString
+// let arr=["Javascript","HTML","CSS","React"]
+// let s=arr.toString()
+// console.log(s)
+
+// // join
+// let arr=["Javascript","HTML","CSS","React"]
+// // if uwant to combine array of string with specified seperator then u can use join
+// console.log(arr.join('|'));
+
+// // concat
+// let a1=[11,12,13]
+// let a2=[14,15,16]
+// let a3=[12,56,78]
+// let newArr=a1.concat(a2,a3) // to combine multiple arrays
+// console.log(newArr)
+
+// // push
+// let a=[10,29,30,40]
+// a.push(60)
+// a.push(70,80,90)// will add element in last
+// console.log(a)
+
+// // unshift
+// let arr=[30,40,50]
+// arr.unshift(10,15) // add in front 
+// console.log(arr) //[ 10, 15, 30, 40, 50 ]
+
+// shift
+// let arr=[23,24,25,26]
+// arr.shift() //delete first element
+// console.log(arr)
+
+// // pop
+// let arr=[23,24,25,26]
+// arr.pop() //delete last element
+// console.log(arr)
+
+// // splice
+// let a=[20,30,40,50]
+// a.splice(1,2) //a,b--> a=>(index) , b=>no. of elements to delete
+// // [20,50]
+// a.splice(1,0,90,100,110) //c-->elements to add
+// //[ 20, 90, 100, 110, 50 ]
+// console.log(a)
+
+// slice
+// let arr=[3,5,7,9,11,13]
+// let res=arr.slice(2,5)
+// console.log(res) //[ 7, 9, 11 ]
+
+// // reverse
+// let arr=[3,5,7,9,11,13]
+// arr.reverse()
+// console.log(arr)
+

71ffa02f27bc123a4ce27e567927cd1f8b6a876a
Harnoorkaur06 <harnoor2925.beai24@chitkara.edu.in>
2026-08-03 19:27:16 +0530

    day 11 added

---
+++ phase_01_htmlcssjs/day_11/index.js
@@ -0,0 +1,10 @@
+// LOCAL STORAGE
+
+// it is a feature of web browser which allows u to store data permanently inside ur system
+
+// {"data":"value"}
+
+// localStorage.setItem("Students_data":"Aman")
+
+// for retrieving data from local storage
+// localStorage.getItem("Students_data") 
